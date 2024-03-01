8dfd652243711675e95653387eddb29dbe88e6b1
NateBergman <90053358+NateBergman@users.noreply.github.com>
2024-03-01 09:53:48 -0800

    second

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        char[] board = new char[64];
-        Arrays.fill(board,8,15,'P');
-        Arrays.fill(board,48,55,'p');
-
+        char[] board = new char[64] {'R','N','B','Q','K','B','N','R','P','P','P','P','P','P','P','P',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','p','p','p','p','p','p','p','p','r','n','b','q','k','b','n','r'};
     }
 }
