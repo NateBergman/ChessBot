ee3ab73e09bf8a31d47e5ffe2da5feef29962f21
NateBergman <90053358+NateBergman@users.noreply.github.com>
2024-03-01 09:46:33 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        char[] board = new char[64];
+        Arrays.fill(board,8,15,'P');
+        Arrays.fill(board,48,55,'p');
+
+    }
+}
