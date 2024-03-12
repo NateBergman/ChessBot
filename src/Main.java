fa2e45c1003de4505385edd91fbaebf9abac4d26
NateBergman <nate@iphid.com>
2024-03-11 22:39:31 -0700

    using bytes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        char[] board = new char[64] {'R','N','B','Q','K','B','N','R','P','P','P','P','P','P','P','P',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','p','p','p','p','p','p','p','p','r','n','b','q','k','b','n','r'};
+        //for each square, byte digits represent __,___,___,enpassant/castling ability(16s),color(8s),3xpiece
+        //0 = empty
+        //1 = pawn
+        //2 = knight
+        //3 = bishop
+        //4 = rook (or 20)
+        //5 = queen
+        //6 = king (or 22)
+        //7 = idk
+        byte[] board = new byte[] {20,2,3,5,22,3,2,20,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,9,9,9,9,9,9,9,28,10,11,13,30,11,10,28};
     }
 }
