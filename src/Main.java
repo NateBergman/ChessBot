961ce6240eb047c06b0938b092e3f0dadc47fcf6
NateBergman <nate@iphid.com>
2024-03-17 22:01:21 -0700

    redoing stuff

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,89 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        //for each square, byte digits represent __,___,___,enpassant/castling ability(16s),color(8s),3xpiece
-        //0 = empty
-        //1 = pawn
-        //2 = knight
-        //3 = bishop
-        //4 = rook (or 20)
-        //5 = queen
-        //6 = king (or 22)
-        //7 = idk
-        byte[] board = new byte[] {20,2,3,5,22,3,2,20,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,9,9,9,9,9,9,9,28,10,11,13,30,11,10,28};
+        char[] board = new char[] {'P','P','P','P','P','P','P','P','R','N','B','Q','K','B','N','R',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '
+                ,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','p','p','p','p','p','p','p','p','r','n','b','q','k','b','n','r'};
+        //board state byte (handles castling and en passant):
+        //En Passant:0-7 represents column, 8+ not viable
+        //Castling: 16s white short, 32s white long, 64s, black short, 128s black long
+        byte boardState = (byte) 240;
+
+        Map<Character,Character> display = buildDisplayMap();
+        printBoard(board,display);
+
+        //'A' = 65
+        //'a' = 97
+        // ' ' = 32
+    }
+    public static ArrayList<int[]> getMoves(char[] board, byte boardState, boolean whiteMove) {
+        if (whiteMove) {
+            for (int i = 0; i < 64; i++) {
+                if (board[i] != ' ' && board[i] < 97) {
+                    if (board[i] == 'R' || board[i] == 'Q') {
+                        //linear moves
+                        //go in +1, -1, +8, -8 increments
+                    }
+                    if (board[i] == 'B' || board[i] == 'Q') {
+                        //diagonal moves
+                        //go in +7, +9, -7, and -9 increments
+                    }
+                    if (board[i] == 'P') {
+                        //pawn moves
+                        //+8 if empty
+                        //+7/+9 if enemy piece or en passant is in that column and currently on rank 5
+                    }
+                    if (board[i] == 'K') {
+                        //king moves
+                        //+7,+8,+9,+1,-1,-7.-8.-9
+                    }
+                    if (board[i] == 'N') {
+                        //knight moves:
+                        //-17
+                        //-15
+                        //-10
+                        //-6
+                        //+6
+                        //+10
+                        //+15
+                        //+17
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    public static void printBoard(char[] board) {
+        for (int i = 7; i > -1; i--) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[8*i + j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void printBoard (char[] board, Map<Character,Character> display) {
+        for (int i = 7; i > -1; i--) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(display.get(board[8*i + j]));
+            }
+            System.out.println();
+        }
+    }
+
+    public static Map<Character,Character> buildDisplayMap () {
+        Map<Character,Character> display = new HashMap<>();
+        display.put(' ',' ');
+        display.put('P','\u265F');
+        display.put('p', '\u2659');
+        display.put('N', '\u265E');
+        display.put('n', '\u2658');
+        display.put('B', '\u265D');
+        display.put('b', '\u2657');
+        display.put('R', '\u265C');
+        display.put('r', '\u2656');
+        display.put('Q', '\u265B');
+        display.put('q', '\u2655');
+        display.put('K', '\u265A');
+        display.put('k', '\u2654');
+        return display;
     }
 }
