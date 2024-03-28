f3d5b9e9ece4ce01adb40bc49da3f93fb64babeb
NateBergman <nate@iphid.com>
2024-03-27 20:59:17 -0700

    cool new move generation from wiki

--- src/SecondDraft.java
+++ src/SecondDraft.java
@@ -1,18 +1,17 @@
 import java.util.*;
-public class SecondDraft { //uses tapered piece-square eval, no/basic pruning, and mailbox with piece lists
+public class SecondDraft { //uses tapered piece-square eval, no/basic pruning, and mailbox with piece lists and offset move generation
     static byte[] board;
     static byte boardState;
     static Set<Integer> whitePieces = new TreeSet<>();
     static Set<Integer> blackPieces = new TreeSet<>();
-    static int[][] diagonalMoves;
-    static int[][] horizontalMoves;
-    static int[] knightMoves;
-    static int[] kingMoves;
     int whiteKing;
     int blackKing;
 
     static int phase;
     static int[] phaseCounts;
+
+    static boolean[] slide = {false,false,true,true,true,false};
+    static int[][] offset = {{},{-21, -19,-12, -8, 8, 12, 19, 21},{-11,  -9,  9, 11},{-10,  -1,  1, 10},{-11, -10, -9, -1, 1,  9, 10, 11},{-11, -10, -9, -1, 1,  9, 10, 11}};
 
     public static void main(String[] args) {
         board = new byte[]{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, //looks upside down in this view, 7s are borders
@@ -31,19 +30,14 @@ public class SecondDraft { //uses tapered piece-square eval, no/basic pruning, a
         Collections.addAll(whitePieces, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38);
         Collections.addAll(blackPieces, 81, 82, 83, 84, 85, 86, 87, 88, 91, 92, 93, 94, 95, 96, 97, 98);
 
-        diagonalMoves = new int[][]{new int[]{11, 22, 33, 44, 55, 66, 77}, new int[]{9, 18, 27, 36, 45, 54, 63}, new int[]{-11, -22, -33, -44, -55, -66, -77}, new int[]{-9, -18, -27, -36, -45, -54, -63}};
-        horizontalMoves = new int[][]{new int[]{10, 20, 30, 40, 50, 60, 70}, new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{-10, -20, -30, -40, -50, -60, -70}, new int[]{-1, -2, -3, -4, -5, -6, -7}};
-        knightMoves = new int[] {21,19,12,8,-8,-12,-19,-21};
-        kingMoves = new int[] {11,10,9,1,-1,-9,-10,-11};
-
         phase = 0;
         phaseCounts = new int[] {0,0,1,1,2,4,0,0,0,0,1,1,2,4,0};
     }
     public static ArrayList<Integer> getWhiteMoves() {
         ArrayList<Integer> moves = new ArrayList<>();
         for (int i : whitePieces) { //i = from coordinate, p = from piece, t = to coordinate, m = to offset, target = to piece
-            byte p = board[i];
-            if (p == 1) { //pawn
+            int p = board[i] - 1;
+            if (p == 0) { //pawn
                 if (i/10 == 8) { //promotions
 
                 }
@@ -52,25 +46,12 @@ public class SecondDraft { //uses tapered piece-square eval, no/basic pruning, a
 
                     }
                 }
-            } else if (p == 2) { //knight
-                for (int m : knightMoves) {
-                    int t = i + m;
-                    byte target = board[t];
-                    if (target == 0 || target > 8) {
-                        moves.add((boardState<<23) + (target<<19) + (i<<8) + (t<<1));
-                    }
-                }
-            } else if (p == 5) { //king
+            } else {
+               int[] o = offset[p];
 
-            } else { //sliding
-                if (p == 4 || p == 5) { //orthogonal
-
-                }
-                if (p == 3 || p == 5) { //diagonal
-
-                }
             }
         }
+        //add castling
         return moves;
     }
 }
