3d3adde9d071c8e7560b51037e2c946bd843a6f0
NateBergman <nate@iphid.com>
2024-03-29 11:01:13 -0700

    have to redo psts because i made them wrong way and white only :(

--- src/PSTMaker.java
+++ src/PSTMaker.java
@@ -3,26 +3,22 @@ import java.util.Arrays;
 public class PSTMaker {
     public static void main (String[] args) {
         int[] pesto = {
-                -74, -35, -18, -18, -11,  15,   4, -17,
-                -12,  17,  14,  17,  17,  38,  23,  11,
-                10,  17,  23,  15,  20,  45,  44,  13,
-                -8,  22,  24,  27,  26,  33,  26,   3,
-                -18,  -4,  21,  24,  27,  23,   9, -11,
-                -19,  -3,  11,  21,  23,  16,   7,  -9,
-                -27, -11,   4,  13,  14,   4,  -5, -17,
-                -53, -34, -21, -11, -28, -14, -24, -43
+                0,   0,   0,   0,   0,   0,  0,   0,
+                98, 134,  61,  95,  68, 126, 34, -11,
+                -6,   7,  26,  31,  65,  56, 25, -20,
+                -14,  13,   6,  21,  23,  12, 17, -23,
+                -27,  -2,  -5,  12,  17,   6, 10, -25,
+                -26,  -4,  -4, -10,   3,   3, 33, -12,
+                -35,  -1, -20, -23, -15,  24, 38, -22,
+                0,   0,   0,   0,   0,   0,  0,   0,
         };
         int offset = 0;
         int[] result = new int[120];
-        for (int y = 0; y < 8; y++) {
+        for (int y = 7; y < -1; y--) {
             for (int x = 0; x < 8; x++) {
-                result[21 + 10 * y + x] = pesto[8 * y + x] + offset;
+                result[91 - 10 * y + x] = pesto[8 * y + x] + offset;
             }
         }
-        int[] invertedResult = new int[120];
-        for (int i = 0; i < 120; i++) {
-            invertedResult[i] = result[119-i];
-        }
-        System.out.print(Arrays.toString(invertedResult));
+        System.out.print(Arrays.toString(result));
     }
 }
