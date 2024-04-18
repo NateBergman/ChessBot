5ba24419c1c4a60e1eeb5da24aa47f164192ed77
NateBergman <90053358+NateBergman@users.noreply.github.com>
2024-04-18 10:54:04 -0700

    ok good progress on ammg

--- src/AttackMapMoveGenTest.java
+++ src/AttackMapMoveGenTest.java
@@ -4,39 +4,75 @@ public class AttackMapMoveGenTest { //also using 0x88
     static byte boardState;
     static Map<Integer,Long> whitePieceMap = new HashMap<>();
     static Map<Integer,Long> blackPieceMap = new HashMap<>();
+    static long whitePieces;
+    static long blackPieces;
+    static long whiteAttacks;
+    static long blackAttacks;
 
-    static boolean[] moveGenSlide = {false,false,true,true,true,false};
-    static int[][] moveGenOffset = {{},{-21, -19,-12, -8, 8, 12, 19, 21},{-11,  -9,  9, 11},{-10,  -1,  1, 10},{-11, -10, -9, -1, 1,  9, 10, 11},{-11, -10, -9, -1, 1,  9, 10, 11}};
+    static boolean[] attackSlide = {false, false,false,true,true,true,false, false,false, false,false,true,true,true,false};
+    static int[][] attackOffset = {{}, {15,17},{-33, -31,-18, -14, 14, 18, 31, 33},{-17,  -15,  15, 17},{-16,  -1,  1, 16},{-17, -15, 15, 17, -16,  -1, 1, 16},{-17, -15, 15, 17, -16,  -1, 1, 16}, {},{},
+            {-15,-17},{-33, -31,-18, -14, 14, 18, 31, 33},{-17,  -15,  15, 17},{-16,  -1,  1, 16},{-17, -15, 15, 17, -16,  -1, 1, 16},{-17, -15, 15, 17, -16,  -1, 1, 16}};
 
     public static void main (String[] args) {
 
     }
-    public static long getAttackMap(int square) {
+    public static long makePieceAttackMap(int square) {
         long attackMap = 0;
-        int fromPiece = board[square] - 1;
-        if (fromPiece == 0) { //pawn
-
-        } else {
-            int[] offsets = moveGenOffset[fromPiece];
-            boolean slide = moveGenSlide[fromPiece];
-            for (int o : offsets) {
-                int to = square + o;
+        int fromPiece = board[square];
+        int[] offsets = attackOffset[fromPiece];
+        boolean slide = attackSlide[fromPiece];
+        for (int o : offsets) {
+            int to = square + o;
                 do {
-                    byte toPiece = board[toCoordinate];
-                    if (toPiece > 8) {
-                        moves.add((boardState << 23) + (fromCoordinate << 8) + (toCoordinate << 1) + (toPiece << 19));
+                    if ((to & 0x88) != 0) {
                         break;
                     }
-                    if (toPiece != 0) {
+                    attackMap += 1 << to;
+                    if (board[to] != 0) {
                         break;
                     }
-                    moves.add((boardState << 23) + (fromCoordinate << 8) + (toCoordinate << 1));
-                    toCoordinate += o;
                 } while (slide);
             }
-        }
+        return attackMap;
     }
-    public static long getMoves(int square) {
-        return
+    public static long makeSideAttackMap(boolean white) {
+        long map = 0;
+        Collection<Long> pieces;
+        if (white) {
+            pieces = whitePieceMap.values();
+        } else {
+            pieces = blackPieceMap.values();
+        }
+        for (long p : pieces) {
+            map = map | p;
+        }
+        return map;
+    }
+    public static long makeSidePieceMap(boolean white) {
+        long map = 0;
+        Set<Integer> pieces;
+        if (white) {
+            pieces = whitePieceMap.keySet();
+        } else {
+            pieces = blackPieceMap.keySet();
+        }
+        for (int p : pieces) {
+            map += 1 << p;
+        }
+        return map;
+    }
+    public static long getPseudoLegalMoves(int square) {
+        byte piece = board[square];
+        if (piece == 1) { //white pawn moves
+            long moves = (whitePieceMap.get(square) & blackPieces);
+        } else if (piece == 9) { //black pawn moves
+            long moves = blackPieceMap.get(square) & whitePieces;
+        } else {
+            if (piece > 8) {
+                return blackPieceMap.get(square) & (~blackPieces);
+            } else {
+                return whitePieceMap.get(square) & (~whitePieces);
+            }
+        }
     }
 }
